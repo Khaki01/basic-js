8bbe8d4ad9925c3787538a6fe92d38ace3fc36d5
Khaki01 <kh.mukashev@gmail.com>
2021-02-11 14:32:25 +0600

    feat: add algorithm

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
+module.exports = function repeater(str, options) {
   // remove line with error and write your code here
+  let res = '';
+  let finalAdder = '';
+  str = String(str);
+  let repTimes = 0;
+  let separator = '+';
+  let addition = '';
+  let additionRepTimes = 0;
+  let additionSeparator = '|';
+  if(options['repeatTimes']) repTimes = options['repeatTimes'];
+  if(options['separator']) separator = options['separator'];
+  if('addition' in options) addition = String(options['addition']);
+  if(options['additionRepeatTimes']) additionRepTimes = options['additionRepeatTimes'];
+  if(options['additionSeparator']) additionSeparator = options['additionSeparator'];
+
+  for(let i = 0; i<additionRepTimes-1; ++i){
+    finalAdder+=addition;
+    finalAdder+=additionSeparator;
+  }
+  finalAdder+=addition;
+
+  for(let i = 0; i<repTimes-1; ++i){
+    res+=str;
+    res+=finalAdder;
+    res+=separator;
+  }
+  res+=str;
+  res+=finalAdder;
+
+  return res;
+
 };
   
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
+  calculateDepth(arr, depth=1) {
     // remove line with error and write your code here
+    for(let i = 0; i<arr.length; ++i){
+      if(Array.isArray(arr[i])){
+        depth++;
+        let a = calculateDepth(arr[i], depth);
+      }
+    }
   }
+
 };
