5e093555fad5d9cee02d7419daff511e475f8037
Khaki01 <kh.mukashev@gmail.com>
2021-02-21 00:43:54 +0600

    feat: add decrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,6 +3,9 @@ const CustomError = require("../extensions/custom-error");
 class VigenereCipheringMachine {
    //notInverted = this.arguments[0];
    //finalWord = '';
+  constructor(notReverse=true){
+    this.machine = notReverse;
+  }
   encrypt() {
     // remove line with error and write your code here
     if(arguments.length!=2) throw new Error('THROWN');
@@ -24,11 +27,10 @@ class VigenereCipheringMachine {
     let ind = 0;
 
     for(let i = 0; i<wordToEncrypt.length; ++i){
-      if(vigenere[wordToEncrypt[i]]){
+      if(codeEncrypt.includes(wordToEncrypt[i], 0)){
         if(ind>secretCode.length-1){ind = 0;}
-        keyWord+= secretCode[ind];
+        keyWord= keyWord.concat(secretCode[ind]);
         ++ind;
-        console.log(ind);
       }
       else{
         keyWord+=wordToEncrypt[i];
@@ -40,7 +42,7 @@ class VigenereCipheringMachine {
 
     let res = '';
     for(let i = 0; i<wordToEncrypt.length; ++i){
-      if(vigenere[wordToEncrypt[i]]) {
+      if(codeEncrypt.includes(wordToEncrypt[i], 0)) {
         let col = vigenere[wordToEncrypt[i]];
         let row = vigenere[keyWord[i]];
         if(col+row>25) res+=codeEncrypt[col+row-26];
@@ -50,12 +52,63 @@ class VigenereCipheringMachine {
         res+=wordToEncrypt[i];
       }
     }
-    return keyWord;
+
+    if(!this.machine)
+      return res.split('').reverse().join('');
+    return res;
     
     
   }    
   decrypt() {
-    throw new CustomError('Not implemented');
+    
+
+    if(arguments.length!=2) throw new Error('THROWN');
+
+    let codeEncrypt = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    
+    let vigenere = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4,
+                    'F':5, 'G':6, 'H':7, 'I':8, 'J':9, 
+                    'K':10, 'L':11, 'M':12, 'N':13, 'O':14, 
+                    'P':15, 'Q':16, 'R':17, 'S':18, 'T':19, 
+                    'U':20, 'V':21, 'W':22, 'X':23, 'Y':24, 'Z':25}
+
+    let wordToDecrypt = String(arguments[0]);
+    let secretCode = String(arguments[1]);
+    
+    secretCode = secretCode.toUpperCase();
+    let keyWord = '';
+    let ind = 0;
+
+    for(let i = 0; i<wordToDecrypt.length; ++i){
+      if(codeEncrypt.includes(wordToDecrypt[i], 0)){
+        if(ind>secretCode.length-1){ind = 0;}
+        keyWord= keyWord.concat(secretCode[ind]);
+        ++ind;
+      }
+      else{
+        keyWord+=wordToDecrypt[i];
+      }
+    }
+    
+    
+    
+
+    let res = '';
+    for(let i = 0; i<wordToDecrypt.length; ++i){
+      if(codeEncrypt.includes(wordToDecrypt[i], 0)) {
+        let row = vigenere[wordToDecrypt[i]];
+        let col = vigenere[keyWord[i]];
+        if(col>row) res+=codeEncrypt[26+row-col];
+        else res+=codeEncrypt[row-col];
+      }
+      else{
+        res+=wordToDecrypt[i];
+      }
+    }
+    if(!this.machine)
+      return res.split('').reverse().join('');
+    return res;
+
     // remove line with error and write your code here
   }
 }
